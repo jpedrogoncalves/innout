fcb78bfc00e9e22e41cff543896e392c20232665
João Pedro <alencargoncalvesj@gmail.com>
2022-09-11 09:19:17 -0300

    primeiros testes com query

--- public/index.php
+++ public/index.php
@@ -1,1 +1,7 @@
-<h1>Ok!</h1>
+<?php
+    require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+
+    // Database::getConnection();
+    $sql = 'select * from users';
+    Database::getResultFromQuery();
+?>
